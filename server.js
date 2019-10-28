944e9deaa02bcacedd1177a18b1d2e257ca86e14
Scottie <avscott@pacbell.net>
2019-10-28 00:28:18 -0700

    add heroku

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 const express = require("express");
+const path = require("path");
+
+
+// Sets up the Express App
+// =============================================================
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//link to routing files
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+// Log (server-side) when our server has started
+console.log("Server listening on: http://localhost:" + PORT);
+});
+
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+  });
