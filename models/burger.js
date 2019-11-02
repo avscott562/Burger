41352c46d225641d9c237535d07239df80be977b
Scottie <avscott@pacbell.net>
2019-11-02 00:51:44 -0700

    add methodsto model

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,26 @@
 // Inside burger.js, import orm.js into burger.js
+let orm = require("../config/orm.js");
 
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+var burger = {
+    all: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.createOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  
 // Export at the end of the burger.js file.
+module.exports = burger;
